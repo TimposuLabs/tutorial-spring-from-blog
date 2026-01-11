113046b8c4bd1fc3676cad9bc7d08d0c827f9713
Ucup Topekox <ucup.topekox@gmail.com>
2026-01-12 00:15:34 +0800

    custom css navigate link if active

--- springboot-mvc-dynamic-layout/src/main/java/com/timposulabs/belajar/controller/DynamicLayoutController.java
+++ springboot-mvc-dynamic-layout/src/main/java/com/timposulabs/belajar/controller/DynamicLayoutController.java
@@ -9,18 +9,21 @@ public class DynamicLayoutController {
 
 	@GetMapping("/")
 	public String home(Model model) {
+		model.addAttribute("page", "home"); // Kirim identitas halaman
 		model.addAttribute("content", "home :: home_content");
 		return "layout";
 	}
 	
 	@GetMapping("/service")
 	public String service(Model model) {
+		model.addAttribute("page", "service"); // Kirim identitas halaman
 		model.addAttribute("content", "service :: service_content");
 		return "layout";
 	}
 	
 	@GetMapping("/contact")
 	public String contact(Model model) {
+		model.addAttribute("page", "contact"); // Kirim identitas halaman
 		model.addAttribute("content", "contact :: contact_content");
 		return "layout";
 	}
